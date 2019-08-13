0d6294fccfce2026dc29d9d7a89f613652cb678b
Thomas Rademaker <Thomas@Rademaker.io>
2019-08-13 19:14:54 -0400

    Initial Commit

---
+++ LittleManComputer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LittleManComputer
+//
+//  Created by SparrowTek on 8/13/19.
+//  Copyright © 2019 SparrowTek LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
