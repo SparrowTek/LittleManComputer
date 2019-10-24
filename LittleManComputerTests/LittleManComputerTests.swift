5c2085ae7c4e11b7143701deff4ee71028238dff
Thomas Rademaker <Thomas@Rademaker.io>
2019-10-24 15:33:50 -0400

    test that program counter has incremented and that the correct print statement has been added

--- LittleManComputerTests/LittleManComputerTests.swift
+++ LittleManComputerTests/LittleManComputerTests.swift
@@ -19,6 +19,21 @@ class LittleManComputerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    func createTestState() -> State {
+        let registers = [507, 108, 902, 901, 109, 902, 000, 001, 010, 003,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
+        000, 000, 000, 000, 000, 000, 000, 000, 000, 000]
+        
+        return State(registers: registers)
+    }
+    
     // MARK: Compiler Tests
     func testCompile() {
         do {
@@ -34,26 +49,36 @@ class LittleManComputerTests: XCTestCase {
                        TEN DAT 010
                        THREE DAT 003
                        """
-            let registers = [507, 108, 902, 901, 109, 902, 000, 001, 010, 003,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000,
-                             000, 000, 000, 000, 000, 000, 000, 000, 000, 000]
+            let testState = createTestState()
             
             let compiler = Compiler()
             let state = try compiler.compile(code)
             
-            XCTAssert(state.registers == registers, "STATE: \(state)")
+            XCTAssert(state.registers == testState.registers, "STATE: \(state)")
         } catch let error {
             XCTAssert(false, "Error: \(error)")
         }
     }
     
     // MARK: Virtual Machine Tests
+    func testVirtualMachineStep() {
+        
+        let state = createTestState()
+        let vm = VirtualMachine(state: state)
+        
+        let expectation = XCTestExpectation(description: "expect State to change from the Virtual Machine \"Step\"")
+        let cancelable = vm.state.sink(receiveCompletion: { completion in
+            expectation.fulfill()
+        }, receiveValue: { state in
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 3)
+        vm.step()
+        XCTAssert(vm.state.value.programCounter == 1, "Program counter should have incremented to 1. \n program counter: \(vm.state.value.programCounter)")
+        let printStatement = "Load the value in register 7 (1) into the accumulator"
+        XCTAssert(vm.state.value.printStatement == printStatement, "wrong print statement: \n \(vm.state.value.printStatement)")
+        XCTAssertNotNil(cancelable, "The subscription should not be nil")
+    }
 
 }
